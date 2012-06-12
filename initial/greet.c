ea8603af5155dcd8c389492361790694ec8a2498
josefelixh <josefelixh@gmail.com>
2012-06-12 23:34:33 +0100

    initial state

---
+++ initial/greet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main () {
+    /* greeting */
+    printf("hi\n");
+
+    /* farewell */
+    printf("bye\n");
+
+    /* all went well */
+    exit(0);
+}
